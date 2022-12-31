f3d7a79f2f8f44bc27b3cdfe244461856a86174a
tomek <tomasz.jan.miskowicz@gmail.com>
2022-12-31 12:21:19 +0100

    sign in

--- zad8-auth/app/src/main/java/com/p_android/zad5_shopping/MainActivity.kt
+++ zad8-auth/app/src/main/java/com/p_android/zad5_shopping/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.p_android.zad5_shopping
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -23,80 +24,40 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         this.onSignInResult(res)
     }
 
-    private fun createSignInIntent() {
+    private fun createSignInIntent(): Intent {
         // [START auth_fui_create_intent]
         // Choose authentication providers
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.GitHubBuilder().build(),
         )
-        // Create and launch sign-in intent
-        val signInIntent = AuthUI.getInstance()
+        return AuthUI.getInstance()
             .createSignInIntentBuilder()
             .setAvailableProviders(providers)
             .build()
-        signInLauncher.launch(signInIntent)
         // [END auth_fui_create_intent]
     }
 
     private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
-        val response = result.idpResponse
-        if (result.resultCode == RESULT_OK) {
-            // Successfully signed in
-            val user = FirebaseAuth.getInstance().currentUser
-            // ...
-        } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
+        if (result.resultCode != RESULT_OK) {
+            val intent = Intent()
+            startActivity(intent)
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+        if (auth.currentUser == null) {
+            signInLauncher.launch(createSignInIntent())
+        }
         supportFragmentManager.beginTransaction().apply {
             add(R.id.fcView, ListFragment.newInstance("a", "b"))
             commit()
         }
-        auth = FirebaseAuth.getInstance()
-
-    }
-    public override fun onStart() {
-        super.onStart()
-
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if(currentUser != null){
-            reload()
-        }
-    }
-
-    private fun reload() {
-        createSignInIntent()
 
     }
 
-    private fun updateUI(user: FirebaseUser?) {
-
-    }
-    private fun signIn(email: String, password: String) {
-        // [START sign_in_with_email]
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithEmail:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                    updateUI(null)
-                }
-            }
-        // [END sign_in_with_email]
-    }
 }
